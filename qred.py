6c7e2e7cf1933e1cee886f1a472edfe20df27b04
087kasumi <68762252+087kasumi@users.noreply.github.com>
2020-08-10 14:39:08 +0900

    Add files via upload

--- qred.py
+++ qred.py
@@ -1,10 +1,10 @@
 import qrcode
 import os
 
-file_name = "qrcode/087qrcode.png"
+file_name = "qrcode/087qrcode_l.png"
 
 qr = qrcode.QRCode(
-    version=12,
+    version=8,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=2,
     border=8
@@ -12,7 +12,7 @@ qr = qrcode.QRCode(
 
 qr.add_data("https://087kasumi.github.io/ohana.github.io/")
 qr.make()
-img = qr.make_image(fill_color="#23dda0", back_color="red")
+img = qr.make_image(fill_color="blue", back_color="red")
 
 img.save(file_name)
 current_dir = os.getcwd()
